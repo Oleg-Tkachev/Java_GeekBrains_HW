601ac3fb8c627ef1e368ce0442f2cffeb24eb10e
Oleg-Tkachev <110471834+Oleg-Tkachev@users.noreply.github.com>
2022-12-16 00:57:55 +0300

    Java_HW_3
    Java_HW_3 
    SortMerge               //  Алгоритм сортировки слиянием
    DellEvenNumbers   //  удаление из массива четных числ
    MinMaxMean         //   В массиве найти минимальное, максимальное и среднее из этого списка.

--- Main.java
+++ Main.java
@@ -9,40 +9,99 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int[] arr = {1, 3, 2, 7, 9, 4, 6, 5, 8};
+        int n = arr.length;
+        System.out.println("Массив до сортировки: ");
+        System.out.println(Arrays.toString(arr));
+        SortMerge(arr, 0, n - 1);
+        System.out.println("Массив после сортировки методом SortMerge: ");
+        System.out.println(Arrays.toString(arr));
+
+
         DellEvenNumbers();
         MinMaxMean();
-
-        //  Реализовать алгоритм сортировки слиянием
-    }
-    private static void SortMerge() {
-
-
-
-
     }
 
-    //  Пусть дан произвольный список целых чисел, удалить из него четные числа
 
-    private static void DellEvenNumbers(){
-        Random rnd = new Random();
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < 20; i++) {
-            list.add(rnd.nextInt(1, 100));
+    //  Реализовать алгоритм сортировки слиянием
+
+    static void SortMerge(int a[], int beg, int end) {
+        if (beg < end) {
+            int mid = (beg + end) / 2;
+            SortMerge(a, beg, mid);
+            SortMerge(a, mid + 1, end);
+            merge(a, beg, mid, end);
         }
-        System.out.println("20 случайных чисел от 1 до 100: ");
-        System.out.println(list);
-        int index = 0;
-        while (index < list.size()) {
-            if (list.get(index) % 2 == 0) {
-                list.remove(index);
+    }
+
+    static void merge(int x[], int y, int mid, int end) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        int n1 = mid - y + 1;
+        int n2 = end - mid;
+
+        int arr1[] = new int[n1];
+        int arr2[] = new int[n2];
+
+        for (i = 0; i < n1; i++)
+            arr1[i] = x[y + i];
+        for (j = 0; j < n2; j++)
+            arr2[j] = x[mid + 1 + j];
+
+        i = 0;
+        j = 0;
+        k = y;
+
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) {
+                x[k] = arr1[i];
+                i++;
             } else {
-                index++;
+                x[k] = arr2[j];
+                j++;
             }
+            k++;
         }
-        System.out.println("Список уже без четных чисел: ");
-        System.out.println(list);
+        while (i < n1) {
+            x[k] = arr1[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            x[k] = arr2[j];
+            j++;
+            k++;
+        }
     }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    //  Дан произвольный список целых чисел, удалить из него четные числа
+
+        private static void DellEvenNumbers () {        //  удаление из массива четных числ
+            Random rnd = new Random();
+            ArrayList<Integer> list = new ArrayList<>();
+            for (int i = 0; i < 20; i++) {
+                list.add(rnd.nextInt(1, 100));
+            }
+            System.out.println("20 случайных чисел от 1 до 100: ");
+            System.out.println(list);
+            int index = 0;
+            while (index < list.size()) {
+                if (list.get(index) % 2 == 0) {
+                    list.remove(index);
+                } else {
+                    index++;
+                }
+            }
+            System.out.println("Список уже без четных чисел: ");
+            System.out.println(list);
+        }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
     //  Задан целочисленный список ArrayList. Найти минимальное, максимальное и среднее из этого списка.
 
@@ -73,3 +132,5 @@ public class Main {
 
     }
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
