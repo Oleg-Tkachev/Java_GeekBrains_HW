dc325b4dbbcd2ea9faa00688bf8e6d681ae9f26e
Oleg-Tkachev <110471834+Oleg-Tkachev@users.noreply.github.com>
2022-12-25 01:14:29 +0300

    FinalTest_JavaGB
    Фильтр ноутбуков для магазина.

---
+++ example/Catalog.java
@@ -0,0 +1,10 @@
+package org.example;
+
+
+public class Catalog extends Laptop {
+
+    public Catalog(int id, int ram, int ssd, String os, String color){
+        super(id,ram,ssd,os,color);
+    }
+
+}
